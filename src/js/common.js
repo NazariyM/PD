4b115843f012efdc2e2252b4a49ec9d9a7cc3f51
NazariyM <nazarriko@gmail.com>
2017-07-14 02:22:55 +0300

    up

--- src/js/common.js
+++ src/js/common.js
@@ -1,15 +1,31 @@
 import dotdotdot from 'dotdotdot';
 // clear placeholder
-(function() {
+(function () {
   const el = $('input, textarea');
-  el.focus(function() {
+  el.focus(function () {
     $(this).data('placeholder', $(this).attr('placeholder'));
     $(this).attr('placeholder', '');
   });
-  el.blur(function() {
+  el.blur(function () {
     $(this).attr('placeholder', $(this).data('placeholder'));
   });
 }());
 
 const $textOverflow = $('.js-dot');
 $textOverflow.dotdotdot();
+
+function initAdvPopups() {
+  const $advPopups = $('.js-advantage-popups');
+  const $advItems = $advPopups.find('.advantage__item');
+  const $advItemCircle = $advPopups.find('.advantage__item-circle');
+
+  $advItemCircle.on('mouseover hover tap', function () {
+    $(this).parent().siblings().addClass('low-index');
+  });
+  $advItemCircle.on('mouseleave', () => {
+    setTimeout(() => {
+      $advItems.removeClass('low-index');
+    }, 50);
+  });
+}
+initAdvPopups();
