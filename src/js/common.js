4d50758283df538e2bb0e853e88d39a8848721a4
NazariyM <nazarriko@gmail.com>
2017-07-14 16:06:41 +0300

    up home

--- src/js/common.js
+++ src/js/common.js
@@ -14,6 +14,7 @@ import dotdotdot from 'dotdotdot';
 const $textOverflow = $('.js-dot');
 $textOverflow.dotdotdot();
 
+// pop-ups z-index in adv section
 function initAdvPopups() {
   const $advPopups = $('.js-advantage-popups');
   const $advItems = $advPopups.find('.advantage__item');
@@ -29,3 +30,31 @@ function initAdvPopups() {
   });
 }
 initAdvPopups();
+
+function toggleBgImg() {
+  const $toggleSection = $('.js-toggle-bg');
+  const $timeBar = $toggleSection.find('.time-bar');
+  const $btnDay = $timeBar.find('.time-bar__btn_day');
+  const $btnNight = $timeBar.find('.time-bar__btn_night');
+  const $bgDay = $toggleSection.find('.bg-day');
+  const $bgNight = $toggleSection.find('.bg-night');
+
+  $bgNight.hide();
+
+  $btnDay.on('click', function (e) {
+    e.preventDefault();
+    $(this).addClass('is-active');
+    $btnNight.removeClass('is-active');
+    $bgDay.fadeIn();
+    $bgNight.fadeOut();
+  });
+
+  $btnNight.on('click', function (e) {
+    e.preventDefault();
+    $(this).addClass('is-active');
+    $btnDay.removeClass('is-active');
+    $bgNight.fadeIn();
+    $bgDay.fadeOut();
+  });
+}
+toggleBgImg();
