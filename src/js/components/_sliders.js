c430535ccb19333adadf4e6d288a953310837be7
NazariyM <nazarriko@gmail.com>
2017-07-15 19:22:21 +0300

    upd aaadte!!!!

--- src/js/components/_sliders.js
+++ src/js/components/_sliders.js
@@ -18,4 +18,22 @@ $aboutSlider.slick($.extend({}, aboutSldDefaults, {
   slidesToShow: 4
 }));
 
+// plan look
+const $planLook = $('.js-plan-look');
+const $status = $('.js-status');
 
+$planLook.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  const i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i);
+});
+
+$planLook.slick({
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  vertical: true,
+  // fade: true,
+  draggable: false,
+  infinite: false,
+  prevArrow: '<button type="button" class="plan__look-btn plan__look-btn_prev"><svg class="plan__look-btn-icon icon-sld-arr"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-sld-arr"></use></svg></button>',
+  nextArrow: '<button type="button" class="plan__look-btn plan__look-btn_next"><svg class="plan__look-btn-icon icon-sld-arr"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-sld-arr"></use></svg></button>'
+});
