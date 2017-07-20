72c61701b1175829c26e932a7a46788edbb0ddb9
NazariyM <nazarriko@gmail.com>
2017-07-20 03:29:18 +0300

    upd

--- src/js/components/_floor-choice.js
+++ src/js/components/_floor-choice.js
@@ -1,32 +1,71 @@
+/* eslint-disable no-trailing-spaces,one-var,prefer-const */
 import { BODY, WINDOW } from '../helpers';
 
-initChoice();
+initFloorChoice();
 
-function initChoice() {
-  const $floorSvg = $('.js-floor-svg');
+function initFloorChoice() {
+  const $svg = $('.js-floor-svg');
+  // if (!$floorSvg.length) return;
 
+  let $path = $svg.find('path'),
+    href = $svg.data('href'),
+    extension = $svg.data('extension');
 
-// check width for IE/Edge and apply it to svg
-  function checkWidth() {
-    const width = WINDOW.width();
-    const height = WINDOW.height();
+  showPathTooltip();
+  hrefPath();
 
-    if (width > 320 && height < 1080) {
-      $floorSvg.attr('width', width);
-      $floorSvg.attr('height', height);
-    }
+  function showPathTooltip() {
+    let tooltip,
+      offset = [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+      ];
+
+    $path.on('mouseenter', function () {
+      let $this = $(this),
+        positionX = $this[0].getBoundingClientRect().left - $svg.offset().left,
+        positionY = $this[0].getBoundingClientRect().top + $(window).scrollTop() - $svg.offset().top;
+
+      tooltip = $(`<div class="floors__tooltip" style="display: none;" data-floor="${$this.data('floor')}">
+<div class="floors__tooltip-block">
+  <span class="floors__tooltip-count">${$this.data('floor')}</span>
+  <div class="floors__tooltip-text">этаж</div>
+</div>
+<div class="floors__tooltip-block">
+  <span class="floors__tooltip-count">${$this.data('sale')}</span>
+  <div class="floors__tooltip-text">в продаже</div>
+</div>
+</div>`);
+      tooltip
+        .insertBefore($svg)
+        .css({
+          top: positionY + +offset[$this.data('floor')],
+          left: positionX - tooltip.outerWidth() + 60 + '%'
+        })
+        .fadeIn(250);
+    });
+
+    $path.on('mouseleave', () => {
+      tooltip.fadeOut(250, function () {
+        $(this).remove();
+      });
+    });
   }
 
-  function detectIE() {
-    const ua = window.navigator.userAgent;
-
-    const msie = ua.indexOf('MSIE');
-    const trident = ua.indexOf('Trident/');
-
-    if (msie > 0 || trident > 0) {
-      WINDOW.on('resize', checkWidth);
-    }
-    return false;
+  function hrefPath() {
+    $path.on('click', function () {
+      window.location.href = href + $(this).data('floor') + '.html';
+      // if (typeof extension !== 'undefined') {
+      //   window.location.href = href + $(this).data('floor') + extension;
+      // }
+    });
   }
-  detectIE();
 }
